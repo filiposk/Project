4ff13e585ca09a98b722e3cd763a88710d2d1c86
filiposk <fpavlovic1995@gmail.com>
2018-08-17 19:07:47 +0200

    filiposk
    Baza

--- Baza/create ingridient-recipe.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Ingredient_Recipe(
-IngredientId int not null,
-RecipeId int not null,
-Quantity decimal(18,2) not null
-);
-
-ALTER TABLE Ingredient_Recipe ADD FOREIGN KEY (IngredientId) references Ingredient(Id);
-ALTER TABLE Ingredient_Recipe ADD FOREIGN KEY (RecipeId) references Recipe(Id);
